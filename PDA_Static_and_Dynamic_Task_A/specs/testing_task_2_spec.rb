58377fc69e99e9091d69611b9bebfc94486a56ff
Joe Stafford <joestafford1986@gmail.com>
2018-05-13 19:26:50 +0100

    added test

--- PDA_Static_and_Dynamic_Task_A/specs/testing_task_2_spec.rb
+++ PDA_Static_and_Dynamic_Task_A/specs/testing_task_2_spec.rb
@@ -2,11 +2,25 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative('../card.rb')
+require_relative('../testing_task_2.rb')
 
 class TestingTask2Test < MiniTest::Test
 
   def setup
-    @card = Card.new("hearts", 1)
+
+    @card1 = Card.new("hearts", 1)
+    @card2 = Card.new("hearts", 1)
+
+    @card_game1 = CardGame.new()
+
+  end
+
+  def test_check_for_ace
+    assert_equal = (true, @card_game1.checkforAce(@card1))
+  end
+
+  def test_highest_card
+
   end
 
 end
